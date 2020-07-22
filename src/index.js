d88c5179f96886e7bbb2d25c131ad73f7fee8c25
Elizabeth Rivera <elizabethriver@gmail.com>
2020-07-22 12:41:15 -0500

    Modificacion de pagina

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import validator from './validator.js';
+ 
+//funcion importa segunda vista
 
-console.log(validator);
+const button1 = document.getElementById('button1');
+button1.addEventListener('click', () => {
+  document.getElementById('mostrar').style.display = 'block';   
+  });
+
+//var userNumInput = document.getElementById("number");
+const button2 = document.getElementById('button2');
+button2.addEventListener('click', () => {
+  const cardNumber= document.getElementById('cardNumber').value;
+  validator.isValid(cardNumber);
+});
+  
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,35 @@
-const validator = {
-  // ...
-};
+  
+  const validator = {
+   
+
+    isValid: function (cardNumber){
+        let credicardNumArray = cardNumber.split("");
+        let sum = 0;
+        let singleNumsArray = []; 
+        let doubleNumsArray = [];
+        let Arrayfinal = undefined;
+        let validCard = false;
+  
+      if (credicardNumArray.length === 16) {
+          for (var index = credicardNumArray.length-1; index >=0; index--) {
+            if (index % 2 !== 0) {
+              singleNumsArray.push(credicardNumArray[index]);
+            } else {
+              doubleNumsArray.push((credicardNumArray[index]*2).toString());
+            }   
+          }
+        }
+        doubleNumsArray = doubleNumsArray.join ("").split("");
+        Arrayfinal = doubleNumsArray.concat (singleNumsArray);
+        for (var j = 0; j < Arrayfinal.length; j++) {
+          sum += parseInt (Arrayfinal[j]);
+        }
+        if (sum % 10 === 0){
+          validCard = true;
+        }
+        return validCard;
+      }
+  
+    }
 
 export default validator;
