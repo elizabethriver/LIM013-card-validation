b83872538a1c67927845fe2e0dd7ce70dabca1ca
Elizabeth Rivera <elizabethriver@gmail.com>
2020-07-27 12:05:43 -0500

    se actualizaron todos los archivos

--- src/index.js
+++ src/index.js
@@ -1,19 +1,56 @@
+   //importar objeto validator
+
 import validator from './validator.js';
  
-//funcion importa segunda vista
-
+//funcion despliega segunda vista
 const button1 = document.getElementById('button1');
-button1.addEventListener('click', () => {
-  document.getElementById('mostrar').style.display = 'block';   
-  });
 
-//var userNumInput = document.getElementById("number");
-const validacion = document.getElementById('validación');
-const mostrarVal = document.getElementById('msj');
+function onClickView (){
+  document.getElementById('mostrar').style.display = 'block';
+  document.getElementById('button1').style.display = 'none';
+}
+button1.addEventListener('click', onClickView); 
+
+//variables a utilizar para la validacion datos de formulario
+const name = document.getElementById('name');
+const lastName = document.getElementById('lastName');
+const mes = document.getElementById('mes');
+const year = document.getElementById('año');
+const cvv = document.getElementById('CVV');
+const numberCard = document.getElementById('cardNumber');
 const button2 = document.getElementById('button2');
-button2.addEventListener('click', () => {
-  let cardNumber= document.getElementById('cardNumber').value;
-  validacion.innerText=validator.isValid(cardNumber);
-  mostrarVal.innerText=validator.maskify(cardNumber);
-});
+const validacion = document.getElementById('validación');
+const mostrarVal = document.getElementById('msj'); 
+const buttonBack = document.getElementById('reload');
+//funcion para validar numero de tarjeta
+function validacionTotal(e) {
+  function goBack (){
+    window.location.reload();
+  } 
+  buttonBack.addEventListener ('click', goBack);
+  
+  const name1 = name.value;
+  const lastName1 = lastName.value;
+  const mes1 = mes.value;
+  const year1 = year.value;
+  const cvv1 = cvv.value;
+//condicion para validar datos del formulario rellenados
+  if (name1==="" || lastName1==="" || mes1==="" || year1 ==="" || cvv1 === "" ) {
+    return false;
+  }
+//condicion para validar número de tarjeta
+  const cardNumber = numberCard.value;    
+  if((!/\d{15,16}(~\W[a-zA-Z])*$/g.test(cardNumber)) || (cardNumber.length > 16)){
+    return false;  
+  }
+  const validacionIsValid = validator.isValid(cardNumber);
+  const validacionMaskify = validator.maskify(cardNumber);  
+  document.getElementById('myForm').style.display = 'none';
+  validacion.innerText= `Tu tarjeta es ${validacionIsValid} `;
+  mostrarVal.innerText= `Tu número de tarjeta termina en los últimos ${validacionMaskify} dígitos`;
+  document.getElementById('h1').style.display = 'none';
+  document.getElementById('p').style.display = 'none';
+  e.preventDefault();
+}
+button2.addEventListener('click', validacionTotal);
   
--- src/validator.js
+++ src/validator.js
@@ -1,17 +1,25 @@
-  
+  //objeto validator
+
   const validator = {
    
-    
-    isValid: function isValid (cardNumber){
+   //metodo isvalid
+   
+    isValid: (cardNumber) => {
+  //definicion de variables locales
+      
         let credicardNumArray = cardNumber.split("");
         let sum = 0;
         let singleNumsArray = []; 
         let doubleNumsArray = [];
         let arrayFinal = undefined;
-        let validCard = "Tu tarjeta no es valida";
-  
+        let validCard = "errónea";
+  //condicionales segun algoritmo de lurhn
+  //Si la longitud del array es igual a 16 se aplica para cada indice, la siguiente condicion
+
         if (credicardNumArray.length === 16) {
-            for (var index = credicardNumArray.length-1; index >=0; index--) {
+  // si el modulo es igual a 0, o par, el indice se añade al array singlenums
+  //En caso contrario se añade al array siglenumbs y se multiplica por 2, convirtiendolo en un string
+          for (var index = credicardNumArray.length-1; index >=0; index--) {
               if (index % 2 !== 0) {
                 singleNumsArray.push(credicardNumArray[index]);
               } else {
@@ -19,28 +27,40 @@
               }   
             }
           }
+  //Se aplica el metodo join para unir elementos de string y luego split para separarlas como array
+  //se construye el array final entre doubleNumsArray y singleNumsArray con metodo concat
+
           doubleNumsArray = doubleNumsArray.join ("").split("");
           arrayFinal = doubleNumsArray.concat (singleNumsArray);
+  //se utiliza for para cada indice del array final, que se convierte en inter y se agrega a la var sum
+        
           for (var j = 0; j < arrayFinal.length; j++) {
             sum += parseInt (arrayFinal[j]);
           }
+  //si la var sum, su modulo entre 10 es igual a 0, se valida la tarjeta, en caso contrario, se rechaza
+
           if (sum % 10 === 0){
-            validCard = "Tu tarjeta es verdadera";
+            validCard = "correcta";
           }
           return validCard;
     },
-
-    maskify: function maskify (cardNumber){
+   //metodo maskify
+    maskify: (cardNumber) => {
+   //se establecen las variables a utilizar 
       let stringCardNumber = cardNumber.toString();
       let lengthCardNumber = stringCardNumber.length;
       let resultadoString = "";
+     //se establece un for para cada indice de la var lengthCardNumber, menos los ultimos 4 digitos, el cual estara reemplazado por el # 
       for (var index = 0; index < lengthCardNumber-4; index++) {
         resultadoString += "#";      
       }
+      //se establece un resultado: valor string de resultadoString + los 4 ultimos digitos visibles
+
       return resultadoString + stringCardNumber.substring(lengthCardNumber-4,lengthCardNumber);     
 
     }
   
   }
+   //exportar objeto validator
 
 export default validator;
